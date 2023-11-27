be6ad94c13eec58cdfc92e290c792a15fdd5b312
d-eniz <zcemdte@ucl.ac.uk>
2023-11-27 22:44:24 +0000

    Final update - further developed on main script

--- normalization_test_temp.m
+++ normalization_test_temp.m
@@ -1,14 +1,40 @@
+clf
 data_array = data_plot;
+max_length = 30; % length of scan in cm
 
-a = 1:length(data_array);
-idx = 1;
-for x = data_array
-    a(2, idx) = 1/(-0.04315*(1-exp(-0.02237*x))+0.3545);
-    idx = idx + 1;
+fluorescence_array(:,1) = [(max_length / length(data_array)) : (max_length / length(data_array)) : max_length];
+fluorescence_array(:,2) = data_array;
+
+for k = 1:length(fluorescence_array)
+    x = fluorescence_array(k,1); % temp
+    y = fluorescence_array(k,2); % temp
+    fluorescence_array(k,3) = fluorescence_array(k,2) * 1/(-0.04315*(1-exp(-0.2237*fluorescence_array(k,1)))+0.3545);
 end
 
-a(2,:) = a(2,:) - mean(a(2,:)) + mean(data_array);
-imagesc(a(2,:));
-colorbar;
-caxis([mean(data_array) - 2.5*std(data_array) mean(data_array) + 2.5*std(data_array)]);
-colormap hot
+stdev_original = std(fluorescence_array(:,2));
+stdev_normalised = std(fluorescence_array(:,3));
+stdev_decrease_percent = abs(100 * (std(fluorescence_array(:,3)) - std(fluorescence_array(:,2)))/std(fluorescence_array(:,2)));
+
+shift = fluorescence_array(1,3)-fluorescence_array(1,2);
+for k = 1:length(fluorescence_array)
+    fluorescence_array(:,4) = fluorescence_array(:,3) - shift;
+end
+
+plot(fluorescence_array(:,1),fluorescence_array(:,2))
+hold on
+plot(fluorescence_array(:,1),fluorescence_array(:,4))
+legend("Original data", "Normalised data")
+text(1,0.305,"Standard deviation decrease: " + stdev_decrease_percent + "%")
+xlabel("Distance (cm)")
+ylabel("Fluorescence (arbitrary units)")
+
+%{
+- Fluorescence array -
+Col 1: Indicates distance in cm
+Col 2: Fluorescence in arbitrary units
+Col 3: Normalised fluorescence data
+Col 4: Normalised data shifted to original data origin
+Col 5: Depth of object at given distance (OpUS data) - i have not done
+this yet
+%}
+
